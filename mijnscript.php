ea806a57aa98113bbb36cca6331f83529dfb5f61
Anna Blaauw <43643959+AnnaBlaauw@users.noreply.github.com>
2018-10-24 16:49:36 +0200

    php oefening

---
+++ mijnscript.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "uw naam is " . $_POST['naam'] . " " . $_POST['achternaam'];  
+
+
+echo "<br> uw leeftijd is " . $_POST['leeftijd'];
+
+
+?>
---
+++ mijnscript2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Uw naam is <?php echo $_POST['naam'] . $_POST['achternaam']; ?><br>
+    Uw leeftijd is <?php echo $_POST['leeftijd']; ?>
+</body>
+</html>
