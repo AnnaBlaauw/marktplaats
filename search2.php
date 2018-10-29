3089762d2eabe0ba41583a4bfd9d4390d71aa351
Anna Blaauw <43643959+AnnaBlaauw@users.noreply.github.com>
2018-10-29 13:40:33 +0100

    eindelijk werkende zoekfunctie

--- search2.php
+++ search2.php
@@ -4,42 +4,30 @@ require_once "config.php";
 $output = '';
 
 if(isset($_POST['search'])) {
-  $search = $_POST['search'];
+  $search = $_POST['query'];
 
 
-$query = mysqli_query ("SELECT * from artikelen WHERE titel LIKE '%$search%' ") or die ("Could not search");
+$query = mysqli_query($link, "SELECT * from artikelen WHERE titel LIKE '%$search%' ") or die ("Could not search");
 
-$count = mysqli_num_rows($query);
+
+$count = mysqli_num_rows($query); 
     
-if($count == 0){
+if ($count == "0") {
   $output = "Geen gevonden resultaten";
 
-}else{
+} else {
 
     while ($row = mysqli_fetch_array($query)) {
-        $titel = $row ['titel'];
-        $text = $row ['text']; 
+        $titel = $row['titel'];
+        $text = $row['text']; 
 
        
-        $output .='<div> '.$titel.''.$text.'</div>';
+        $output ='<div>' .$titel. $text. '</div><br>';
     }
 }
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
-</head>
-<body>
-    <form action="<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]); ?>" method="GET">
-        <input type="text" name="query" />
-        <input type="submit" value="Search" />
-</form>
-<?php print ("$output");?>
-</body>
-</html>
+echo "test" . $output;
+
+
+
